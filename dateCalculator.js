38dea8cd963d5d1f9f8825237bedb9dee223b685
TitasKanti <adv.titas.bd@gmail.com>
2021-09-11 12:11:32 +0600

    final UI update

--- dateCalculator.js
+++ dateCalculator.js
@@ -1,10 +1,10 @@
 document.getElementById('calculate').addEventListener('click', function () {
-
+    //getting the user inputs
     const firstDate = document.getElementById('first-date');
     const start = firstDate.value;
 
     const lastDate = document.getElementById('last-date');
-    const end = lastDate.value; console.log(end);
+    const end = lastDate.value;
 
     const principleAmount = document.getElementById('amount');
     const amount = parseFloat(principleAmount.value);
@@ -15,8 +15,14 @@ document.getElementById('calculate').addEventListener('click', function () {
     //display result
     const div = document.getElementById('result');
     div.innerHTML = `
-    <h1>The Interest is ${result} </h1>
+    <h1 class = "design">The Interest is ${result} BDT. </h1>
     `;
+
+    //clear inputs
+    firstDate.value = '';
+    lastDate.value = '';
+    principleAmount.value = '';
+
 })
 const getInterest = (start, end, amount) => {
     const date1 = new Date(start);
@@ -26,13 +32,14 @@ const getInterest = (start, end, amount) => {
     const oneDay = 1000 * 60 * 60 * 24;
 
     // Calculating the time difference between two dates
-    const diffInTime = date2.getTime() - date1.getTime();
+    const timeDifference = date2.getTime() - date1.getTime();
 
     // Calculating the no. of days between two dates
-    const diffInDays = Math.round(diffInTime / oneDay);
+    const diffInDays = Math.round(timeDifference / oneDay);
     const interest = amount * 0.12 * diffInDays / 365;
 
-    return interest.toFixed(4);
+    return Math.abs(interest.toFixed(3));
 }
-
-
+const reload = () => {
+    location.reload();
+}
